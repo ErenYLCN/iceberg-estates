92847d82c97524387ea195638b0201540373dc02
ErenYLCN <yalcineren97@gmail.com>
2021-08-17 14:40:15 +0300

    airtable files are created

---
+++ src/airtable/agents/agents.js
@@ -0,0 +1,7 @@
+import { ref } from "vue";
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "key8nnBpvbIRer3vC" }).base(
+	"appgykZBGTF92MnHu"
+);
+
+export const getAgents = () => {};
---
+++ src/airtable/appointments/appointments.js
@@ -0,0 +1,11 @@
+import { ref } from "vue";
+var Airtable = require("airtable");
+var base = new Airtable({ apiKey: "key8nnBpvbIRer3vC" }).base(
+	"appgykZBGTF92MnHu"
+);
+
+export const getAppointments = () => {};
+
+export const createAppointment = () => {};
+
+export const updateAppointment = () => {};
